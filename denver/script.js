b7ace32b81bae43e8698d4c18d1bfd7907d4e894
sef-ninja <email@email.com>
2016-06-25 20:44:06 -0500

    Show day names

--- denver/script.js
+++ denver/script.js
@@ -5,6 +5,7 @@ app.controller("HelloController", function($scope, $http) {
 	$scope.message = "initial value";
     $http.get('plan.json').success(function(data){
         plan = data;  
-        $scope.message = plan.days[0].name;
+        $scope.message = "success!";
+        $scope.days = data.days;
     }); 
 });
