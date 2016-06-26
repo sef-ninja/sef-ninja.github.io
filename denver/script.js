133fac207877402ace619832dc8bcab881204219
sef-ninja <email@email.com>
2016-06-25 19:46:43 -0500

    Injected http service into controller

--- denver/script.js
+++ denver/script.js
@@ -1,7 +1,7 @@
 
 var plan = null;
 var app = angular.module("app", []);
-app.controller("HelloController", function($scope) {
+app.controller("HelloController", function($scope, $http) {
 	$scope.message = "initial value";
     $http.get('plan.json').success(function(data){
         plan = data;  
