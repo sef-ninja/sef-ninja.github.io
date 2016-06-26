d8842da966e49ee4ca36ee31cb35d21427ae8006
somename <hourglass@hourglass.(none)>
2016-06-26 09:41:31 -0500

    Updated plan.json relative path

--- denver/script.js
+++ denver/script.js
@@ -3,7 +3,7 @@ var plan = null;
 var app = angular.module("app", []);
 app.controller("HelloController", function($scope, $http) {
 	$scope.message = "Denver Trip Plan";
-    $http.get('plan.json').success(function(data){
+    $http.get('denver/plan.json').success(function(data){
         plan = data;  
         $scope.days = data.days;
     }); 
