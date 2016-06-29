4505077f5924a72fd57fcec101a7670ded01bf84
sef-ninja <email@email.com>
2016-06-28 20:05:38 -0500

    Fixed relative paths

--- denver/script.js
+++ denver/script.js
@@ -3,7 +3,7 @@
 var app = angular.module("app", []);
 app.controller("HelloController", function($scope, $http) {
 	$scope.message = "Denver Trip Plan";
-    $http.get('denver/plan.json').success(function(data){
+    $http.get('plan.json').success(function(data){
         $scope.days = data.days;
     }); 
 });
