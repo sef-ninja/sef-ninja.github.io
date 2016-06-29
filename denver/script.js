068e26562e8b80715c974524a671f057ea550a6c
sef-ninja <email@email.com>
2016-06-28 20:03:33 -0500

    Troubleshooting plan

--- denver/script.js
+++ denver/script.js
@@ -1,10 +1,9 @@
 
-var plan = null;
+
 var app = angular.module("app", []);
 app.controller("HelloController", function($scope, $http) {
 	$scope.message = "Denver Trip Plan";
     $http.get('denver/plan.json').success(function(data){
-        plan = data;  
         $scope.days = data.days;
     }); 
 });
