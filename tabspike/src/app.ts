ae507f21640663df4e7577352c9ad63c092f981e
somename <hourglass@hourglass.(none)>
2016-07-02 10:31:57 -0500

    Added spike for reading json

--- tabspike/src/app.ts
+++ tabspike/src/app.ts
@@ -1,5 +1,7 @@
 //our root app component
 import {Component} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Rx';
 import {Tabs} from './tabs';
 import {Tab} from './tab';
 
